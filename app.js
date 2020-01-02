a5649dd7eb4ed92ae3b84c7e9595aebb5e0ad634
Arkadiusz Zygan <a.zygan@gmail.com>
2020-01-02 12:40:38 +0100

    Add data

--- app.js
+++ app.js
@@ -1,7 +1,82 @@
 const fs = require('fs');
 
+const genders = [
+    'male',
+    'female',
+];
 
-fs.writeFile('outputfile.txt', data, (err) => {
+const maleNames = [
+    'Rohan',
+    'Kenyon',
+    'Jeffery',
+    'Bernard',
+    'Humberto',
+    'Tate',
+    'Dexter',
+    'Luke',
+    'Julien',
+    'Aarav',
+    'Luca',
+    'Malcolm',
+    'Niko',
+    'Braylen',
+    'Orion',
+    'Aydan',
+    'Scott',
+    'Shaun',
+    'Lennon',
+    'Darryl',
+]; 
+const femaleNames = [
+    'Elsie',
+    'Justine',
+    'Liana',
+    'Saniyah',
+    'Simone',
+    'Larissa',
+    'Regina',
+    'Rory',
+    'Krista',
+    'Jolie',
+    'Athena',
+    'Destiney',
+    'Gina',
+    'Crystal',
+    'Lilia',
+    'Arielle',
+    'Julianna',
+    'Mallory',
+    'Miriam',
+    'Jamie',
+];
+
+const lastNames = [
+    'Santiago',
+    'Cross',
+    'Powell',
+    'Phillips',
+    'Blankenship',
+    'Lynch',
+    'Arias',
+    'Duarte',
+    'Fox',
+    'Mcdonald',
+    'Clay',
+    'Shelton',
+    'Dudley',
+    'Wise',
+    'Perez',
+    'Harrington',
+    'Velasquez',
+    'Kaiser',
+    'Stuart',
+    'Howard',
+];
+
+people = [];
+
+
+fs.writeFile('people.json', data, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
